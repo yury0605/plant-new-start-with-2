5f0c72b0a1712d559a1913f67f15a29c4fe0d5b7
Yury <sendtopepper@gmail.com>
2023-02-04 14:07:15 +0300

    done all tasks

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 const burgerItem = document.querySelector('.header_burger');
 const menu = document.querySelector(".header_nav");
 const menuCloseItem = document.querySelector('.header_nav_close');
-const menuCloseLink = document.querySelectorAll('.header_link');
+const menuCloseLink = document.querySelector('.header_list');
 burgerItem.addEventListener('click', () => {
   menu.classList.add('header_nav_active');
     });
